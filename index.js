80fc56f491ff6470c9a383371ef160041a8062c5
Tasif <taz181995@gmail.com>
2021-05-14 08:12:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(String){
+    cats.push(String);
+}
+
+function destructivelyPrependCat(String){
+    cats.unshift(String);
+}
+
+function destructivelyRemoveFirstCat(String){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(String){
+    cats.pop();
+}
+
+function removeFirstCat(){
+    cats.splice(0,1);
+}
+
+function removeLastCat(){
+    cats.splice(-1,1);
+}
+
+function appendCat(){
+    
+}
+
+function prependCat(){
+
+}
